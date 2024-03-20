23dbc6640a32fb999f9b2694258afa610ff5b524
Matthew Black <matthewallenblack@gmail.com>
2024-03-20 15:23:38 -0500

    tweak instruction verbiage

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// 1. Start with the number 29 and set that equal to a variable named highScore;
+// 1. Create a variable named highScore and assign it a number value of 29.
 
 
 // 2. Make a new empty array called myScrabbleTiles.
 
 
-// 3. Put these 6 tile objects into myScrabbleTiles:
+// 3. Put these 6 tile objects into myScrabbleTiles.
 /*
   { tile: 'N', score: 1 }
   { tile: 'K', score: 5 }
@@ -18,22 +18,24 @@
 // 4. Remove the last tile from myScrabbleTiles and save it in a variable named removedTile.
 
 
-// 5. Add the following new tile to myScrabbleTiles:
+// 5. Add the following new tile object to myScrabbleTiles.
 /*
   { tile: 'F', score : 4 }
 */
 
 
-// 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop to add up and return the sum of all the scores for a given array of scrabble tiles.
+// 6. Complete this function. It needs to be given an array of tile objects. The function will use a loop to add up
+//    and return the sum of all the scores for a given array of scrabble tiles.
 function calculateScore(tiles) {
 
 }
 
 
-// 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
+// 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called myScore.
 
 
-// 8. Check whether or not your score is higher than the highScore. If your score is higher, change highScore to the new high score.
+// 8. Write code to check whether or not your score is higher than the highScore.
+//    If your score is higher, change highScore to the new high score.
 
 
 // DONE!
