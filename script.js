e12559b4cdca3d0929f7b771d8f8b230e81250b6
Brad Flaherty <makingbrad@gmail.com>
2024-08-31 21:37:47 -0500

    done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // 1. Create a variable named highScore and assign it a number value of 29.
-
+let highScore = 29;
 
 // 2. Make a new empty array called myScrabbleTiles.
-
+myScrabbleTiles =[];
 
 // 3. Put these 6 tile objects into myScrabbleTiles.
 /*
@@ -13,32 +13,54 @@
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
-
+myScrabbleTiles = [
+  { tile: 'N', score: 1 },
+  { tile: 'K', score: 5 },
+  { tile: 'Z', score: 10 },
+  { tile: 'X', score: 8 },
+  { tile: 'D', score: 2 },
+  { tile: 'A', score: 1 },
+]
 
 // 4. Remove the last tile from myScrabbleTiles and save it in a variable named removedTile.
 
+let indexOfLast = myScrabbleTiles.length-1
+
+let removedTile=myScrabbleTiles[indexOfLast];
+
+myScrabbleTiles.pop()
+
+
 
 // 5. Add the following new tile object to myScrabbleTiles.
-/*
-  { tile: 'F', score : 4 }
-*/
+
+myScrabbleTiles.push(  { tile: 'F', score : 4 });
+
 
 
 // 6. Complete this function. It expects to receive an array of tile objects. The function will 
 //    use a loop to calculate the sum of the tile objects' scores. The function should return
 //    the total sum.
-function calculateScore(tiles) {
-
+function calculateScore(array) {
+  let totalSum=0
+  for (let i = 0; i < myScrabbleTiles.length; i++) {
+    totalSum = totalSum + myScrabbleTiles[i].score;
+    
+  }
+return totalSum
 }
 
 
 // 7. Use the function above to calculate the total score for myScrabbleTiles and assign that value
 //    to a new variable called myScore.
+let myScore = calculateScore(myScrabbleTiles);
 
 
 // 8. Write code to check whether or not your score is higher than the highScore.
 //    If your score is higher, change highScore to the new high score.
-
+if (myScore>highScore) {
+  highScore=myScore
+}else{}//do nothing
 
 // DONE!
 
